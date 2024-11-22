d81dc61a9e75fe423dc3ff6da3110af980772dda
Mohamed Hamdan <hamdan41d@gmail.com>
2024-11-21 19:49:23 -0600

    Updated File/ Added Main

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,11 +1,13 @@
 import "./App.css";
 import Header from "../Header/Header";
+import Main from "../Main/Main";
 
 function App() {
   return (
     <div className="page">
       <dir className="page__content">
         <Header />
+        <Main />
       </dir>
     </div>
   );
