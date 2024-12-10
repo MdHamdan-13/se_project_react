7ef03221bcbf980a0f09c1a83d4adbb95495ce2a
Mohamed Hamdan <hamdan41d@gmail.com>
2024-12-09 18:25:57 -0600

    Added ToggleSwitch CSS

--- src/components/ToggleSwitch/ToggleSwith.jsx
+++ src/components/ToggleSwitch/ToggleSwith.jsx
@@ -1,11 +1,31 @@
-import React from "react";
+import React, { useContext, useState } from "react";
 import "./ToggleSwitch.css";
 
 function ToggleSwitch() {
-  console.log("toggle");
+  const [currentTempUnit, handleToggleChange] = useState("C");
+
+  const handleChange = (event) => {
+    if (currentTempUnit === "C") handleToggleChange("F");
+    if (currentTempUnit === "F") handleToggleChange("C");
+  };
+
+  console.log(currentTempUnit);
   return (
     <label htmlFor="" className="switch">
-      <input type="checkbox" className="switch__checkBox" />
+      <input
+        type="checkbox"
+        className="switch__checkBox"
+        onChange={handleChange}
+      />
+      <span
+        className={
+          currentTempUnit === "F"
+            ? "switch__slider switch__slider-F"
+            : "switch__slider-C"
+        }
+      ></span>
+      <p className="switch__temp-F">F</p>
+      <p className="switch__temp-C">C</p>
     </label>
   );
 }
