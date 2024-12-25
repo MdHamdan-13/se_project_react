6f7280cf5cb9dfd15469265ec1a0e0f6da36bfd9
Mohamed Hamdan <hamdan41d@gmail.com>
2024-12-25 16:54:45 -0600

    Update

--- src/components/AddItemModal/AddItemModal.jsx
+++ src/components/AddItemModal/AddItemModal.jsx
@@ -1,6 +1,5 @@
 import React, { useEffect, useState } from "react";
 import ModalWithForm from "../ModalWithForm/ModalWithForm";
-import "./AddItemModal.css";
 
 const AddItemModal = ({ isOpen, onAddItem, closeModal }) => {
   const [name, setName] = useState("");
