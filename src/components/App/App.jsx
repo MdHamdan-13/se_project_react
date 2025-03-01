f8240b3a3e903023f1421eb7d8dff94202dffb43
Mohamed Hamdan <hamdan41d@gmail.com>
2025-03-01 11:31:53 -0600

    Update in App.jsx

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -29,6 +29,7 @@ function App() {
   const [currentTemperatureUnit, setCurrentTemperatureUnit] = useState("F");
   const [clothingItems, setClothingItems] = useState([]);
   const [isLoggedIn, setIsLoggedIn] = useState(false);
+  const [currentUser, setCurrentUser] = useState();
 
   const handleCardClick = (card) => {
     setActiveModal("preview");
